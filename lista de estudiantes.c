76295f88a8dee730dd0300027384e511173d584d
Mati <amaticc03@gmail.com>
2023-07-10 15:43:51 -0500

    se corrigieron errores

--- lista de estudiantes.c
+++ lista de estudiantes.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ESTUDIANTES 100
+#define MAX_ALUMNOS 100
 
-struct Estudiante {
+struct Alumno {
     int codigo;
     char nombre[50];
     char carrera[50];
@@ -20,23 +20,23 @@ float calcular_promedio(float* notas, int cantidad) {
     return suma / cantidad;
 }
 
-void ordenar_estudiantes(struct Estudiante* estudiantes, int cantidad) {
+void ordenar_alumnos(struct Alumno* alumnos, int cantidad) {
     int i, j;
-    struct Estudiante temp;
+    struct Alumno temp;
 
     for (i = 0; i < cantidad - 1; i++) {
         for (j = 0; j < cantidad - i - 1; j++) {
-            if (estudiantes[j].codigo < estudiantes[j + 1].codigo) {
-                temp = estudiantes[j];
-                estudiantes[j] = estudiantes[j + 1];
-                estudiantes[j + 1] = temp;
+            if (alumnos[j].codigo < alumnos[j + 1].codigo) {
+                temp = alumnos[j];
+                alumnos[j] = alumnos[j + 1];
+                alumnos[j + 1] = temp;
             }
         }
     }
 }
 
-void escribir_estudiantes(struct Estudiante* estudiantes, int cantidad, float promedio) {
-    FILE* archivoSalida = fopen("estudiantes_ordenados.txt", "w");
+void escribir_alumnos(struct Alumno* alumnos, int cantidad, float promedio) {
+    FILE* archivoSalida = fopen("alumnos_ordenados.txt", "w");
     if (archivoSalida == NULL) {
         printf("Error al abrir el archivo.\n");
         return;
@@ -45,9 +45,9 @@ void escribir_estudiantes(struct Estudiante* estudiantes, int cantidad, float pr
     fprintf(archivoSalida, "Codigo\t\tNombre\t\t\tCarrera\t\t\tNotas\n");
     int i, j;
     for (i = 0; i < cantidad; i++) {
-        fprintf(archivoSalida, "%d\t\t%s\t\t%s\t\t", estudiantes[i].codigo, estudiantes[i].nombre, estudiantes[i].carrera);
+        fprintf(archivoSalida, "%d\t\t%s\t\t%s\t\t", alumnos[i].codigo, alumnos[i].nombre, alumnos[i].carrera);
         for (j = 0; j < 3; j++) {
-            fprintf(archivoSalida, "%.2f\t", estudiantes[i].notas[j]);
+            fprintf(archivoSalida, "%.2f\t", alumnos[i].notas[j]);
         }
         fprintf(archivoSalida, "\n");
     }
@@ -57,7 +57,7 @@ void escribir_estudiantes(struct Estudiante* estudiantes, int cantidad, float pr
 }
 
 int main() {
-    struct Estudiante estudiantes[MAX_ESTUDIANTES];
+    struct Alumno alumnos[MAX_ALUMNOS];
     int cantidad = 0;
 
     FILE* archivoEntrada = fopen("alumnos.txt", "r");
@@ -66,16 +66,16 @@ int main() {
         return 1;
     }
 
-    while (fscanf(archivoEntrada, "%d;%[^;];%[^;];%f;%f;%f", &estudiantes[cantidad].codigo, estudiantes[cantidad].nombre, estudiantes[cantidad].carrera,
-                  &estudiantes[cantidad].notas[0], &estudiantes[cantidad].notas[1], &estudiantes[cantidad].notas[2]) != EOF) {
+    while (fscanf(archivoEntrada, "%d;%[^;];%[^;];%f;%f;%f", &alumnos[cantidad].codigo, alumnos[cantidad].nombre, alumnos[cantidad].carrera,
+                  &alumnos[cantidad].notas[0], &alumnos[cantidad].notas[1], &alumnos[cantidad].notas[2]) != EOF) {
         cantidad++;
     }
 
     fclose(archivoEntrada);
 
-    ordenar_estudiantes(estudiantes, cantidad);
-    float promedio = calcular_promedio(estudiantes[0].notas, 3);
-    escribir_estudiantes(estudiantes, cantidad, promedio);
+    ordenar_alumnos(alumnos, cantidad);
+    float promedio = calcular_promedio(alumnos[0].notas, 3);
+    escribir_alumnos(alumnos, cantidad, promedio);
 
     printf("Archivo generado exitosamente.\n");
 
