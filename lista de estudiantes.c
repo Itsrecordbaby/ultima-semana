84c8ec48d6b6e3d6a41b1a15aab7cbfc404fdd80
Mati <amaticc03@gmail.com>
2023-07-10 15:06:56 -0500

    se agrego una estrctura para los estudiantes

--- lista de estudiantes.c
+++ lista de estudiantes.c
@@ -1,4 +1,26 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_ESTUDIANTES 5
+
+struct Estudiante {
+    int codigo;
+    char nombre[50];
+    char carrera[50];
+    float notas[3];
+};
+
+float calcular_promedio(float* notas, int cantidad) {
+    float suma = 0;
+    int i;
+    for (i = 0; i < cantidad; i++) {
+        suma += notas[i];
+    }
+    return suma / cantidad;
+}
+
+
 
 
